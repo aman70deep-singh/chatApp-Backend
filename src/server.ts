c233bb8ca5040682a747953bff974d8884ea8856
amandeepsingh-art <aman.zecotok@gmail.com>
2025-12-09 11:15:36 +0530

    socket.io integeration

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,15 @@
 import http from 'http';
 import app from './app';
-import { Server } from "socket.io";
+import { initSocket } from './socket/socket';
 import { connectToDatabase } from './db';
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-    }
-})
+initSocket(server);
 
-io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
-})
+
 connectToDatabase().then(() => {
     console.log("Database connected successfully");
     server.listen(PORT, () => {
---
+++ src/socket/socket.ts
@@ -0,0 +1,52 @@
+
+import { Server } from "socket.io";
+
+let io: Server;
+
+export const initSocket = (httpServer: any) => {
+    io = new Server(httpServer, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    });
+
+    io.on("connection", (socket) => {
+        console.log("New user connected:", socket.id);
+
+        socket.on("setup", (userData) => {
+            socket.join(userData._id);
+            socket.emit("connected");
+            console.log("User room joined:", userData._id);
+        });
+
+        socket.on("join-chat", (chatId) => {
+            socket.join(chatId);
+            console.log("User joined chat:", chatId);
+        });
+
+        socket.on("typing", (chatId) => {
+            socket.to(chatId).emit("typing");
+        });
+
+        socket.on("stop-typing", (chatId) => {
+            socket.to(chatId).emit("stop-typing");
+        });
+
+        socket.on("send-message", (message) => {
+            const chat = message.chatId;
+            if (!chat) return console.log("Chat not found in message event");
+
+            socket.to(chat).emit("receive-message", message);
+            console.log("Message sent in room:", chat);
+        });
+
+        socket.on("disconnect", () => {
+            console.log("User disconnected:", socket.id);
+        });
+    });
+
+    return io;
+};
+
+export const getIO = () => io;
