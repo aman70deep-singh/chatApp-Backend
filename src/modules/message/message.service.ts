15c8329f1f9dd6abc78db02fbedaaa8f70e57392
amandeepsingh-art <aman.zecotok@gmail.com>
2026-01-24 18:15:51 +0530

    optimize getMessage api using redis

--- src/modules/message/message.service.ts
+++ src/modules/message/message.service.ts
@@ -3,6 +3,7 @@ import ChatModel from "../../models/chat.model";
 import MessageModel from "../../models/message.model";
 import { getIO } from "../../socket/socket";
 import { Types } from "mongoose";
+import redisClient from "../../redis";
 export async function sendMessage(senderId: string, data: any, file?: any) {
   const chat = await ChatModel.findById(data.chatId);
   if (!chat) {
@@ -51,6 +52,13 @@ export async function sendMessage(senderId: string, data: any, file?: any) {
   const io = getIO();
   io.to(data.chatId).emit("message-received", createdMessage);
 
+  try {
+    const cacheKey = `messages:${data.chatId}:latest`;
+    await redisClient.del(cacheKey);
+  } catch (err) {
+    console.error("Redis Cache Invalidation error:", err);
+  }
+
   return createdMessage;
 }
 
@@ -58,26 +66,47 @@ export async function getMessages(
   chatId: string,
   cursor?: string,
   limit: number = 20
-
 ) {
+  const cacheKey = `messages:${chatId}:latest`;
+  const isLatestPage = !cursor;
+
+  if (isLatestPage) {
+    try {
+      const cachedData = await redisClient.get(cacheKey);
+      if (cachedData) {
+        return JSON.parse(cachedData);
+      }
+    } catch (err) {
+      console.error("Redis Get error:", err);
+    }
+  }
+
   const query: any = { chatId };
   if (cursor) {
     query._id = { $lt: new Types.ObjectId(cursor) };
   }
-  const messages = await MessageModel
-    .find(query)
+  const messages = await MessageModel.find(query)
     .populate("sender", "name  profilePic")
     .sort({ _id: -1 })
     .limit(limit + 1);
   const hasNextPage = messages.length > limit;
   const results = hasNextPage ? messages.slice(0, limit) : messages;
 
-  return {
+  const response = {
     messages: results,
     nextCursor: hasNextPage ? results[results.length - 1]?._id.toString() : null,
-    hasNextPage
+    hasNextPage,
   };
 
+  if (isLatestPage) {
+    try {
+      await redisClient.setEx(cacheKey, 300, JSON.stringify(response));
+    } catch (err) {
+      console.error("Redis Set error:", err);
+    }
+  }
+
+  return response;
 }
 
 export async function uploadImage(file: any) {
