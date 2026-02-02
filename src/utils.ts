56c688f597edf08c22efb79a229371711dadaa75
amandeepsingh-art <aman.zecotok@gmail.com>
2026-02-02 19:30:49 +0530

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@ export const generateOtp = () => {
 const transport = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, 
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
